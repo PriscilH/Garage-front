f90999cd5b38eb2d70752c1c24086a6dc1203794
Priscilla <comptepro@mbpdepriscilla.home>
2023-06-02 20:35:42 +0200

    add filters

--- pieces.js
+++ pieces.js
@@ -2,6 +2,7 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
+function genererPieces(pieces){
 for (let i = 0; i < pieces.length; i++) {
 
     const article = pieces[i];
@@ -9,7 +10,6 @@ for (let i = 0; i < pieces.length; i++) {
     const sectionFiches = document.querySelector(".fiches");
     // Création d’une balise dédiée à une pièce automobile
     const pieceElement = document.createElement("article");
-
 // Création des balises 
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
@@ -39,6 +39,9 @@ pieceElement.appendChild(descriptionElement);
 pieceElement.appendChild(stockElement);
 
 }
+}
+
+genererPieces(pieces);
 
 //gestion des bouttons 
 const boutonTrier = document.querySelector(".btn-trier");
@@ -48,7 +51,8 @@ const piecesOrdonnees = Array.from(pieces);
 piecesOrdonnees.sort(function (a, b) {
     return a.prix - b.prix;
  });
- console.log(piecesOrdonnees);
+ document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
 });
 
 const boutonFiltrer = document.querySelector(".btn-filtrer");
@@ -57,7 +61,8 @@ boutonFiltrer.addEventListener("click", function () {
 const piecesFiltrees = pieces.filter(function (piece) {
     return piece.prix <= 35;
 });
-console.log(piecesFiltrees)
+document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
 });
 
 //Correction Exercice
@@ -68,7 +73,8 @@ const piecesOrdonnees = Array.from(pieces);
 piecesOrdonnees.sort(function (a, b) {
     return b.prix - a.prix;
  });
- console.log(piecesOrdonnees);
+ document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
 });
 
 const boutonNoDescription = document.querySelector(".btn-nodesc");
@@ -77,5 +83,62 @@ boutonNoDescription.addEventListener("click", function () {
 const piecesFiltrees = pieces.filter(function (piece) {
     return piece.description
 });
-console.log(piecesFiltrees)
+document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
 });
+
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+console.log(noms)
+//Création de l'en-tête
+
+const pElement = document.createElement('p')
+pElement.innerText = "Pièces abordables";
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement);
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(pElement)
+    .appendChild(abordablesElements)
+
+//Code Exercice 
+const nomsDisponibles = pieces.map(piece => piece.nom);
+const prixDisponibles = pieces.map(piece => piece.prix);
+
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice(i,1);
+        prixDisponibles.splice(i,1);
+    }
+}
+
+const disponiblesElement = document.createElement('ul');
+
+for(let i=0 ; i < nomsDisponibles.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`;
+    disponiblesElement.appendChild(nomElement);
+}
+
+const pElementDisponible = document.createElement('p')
+pElementDisponible.innerText = "Pièces disponibles:";
+document.querySelector('.disponibles').appendChild(pElementDisponible).appendChild(disponiblesElement)
+
+const inputPrixMax = document.querySelector('#prix-max')
+inputPrixMax.addEventListener('input', function(){
+    const piecesFiltrees = pieces.filter(function(piece){
+        return piece.prix <= inputPrixMax.value;
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);  
+})
